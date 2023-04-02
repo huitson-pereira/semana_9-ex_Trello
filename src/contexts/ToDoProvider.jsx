15dceda06775572c9159372f53fd1d7c7f57f17a
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-04-01 22:05:23 -0300

    Ex.6 - Extraindo a lógica para o contexto

--- src/contexts/ToDoProvider.jsx
+++ src/contexts/ToDoProvider.jsx
@@ -1,4 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+//export const Home = () => {
+    const [toDos, setToDos] = useState([
+      { id: 1, text: "Estudar React", done: false },
+      { id: 2, text: "Praticar com um projeto", done: true },
+      { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
+    ]);
+  
+    const addToDo = (text) => {
+      const newToDo = {
+        id: Date.now(),
+        text,
+        done: false,
+      };
+      setToDos([...toDos, newToDo]);
+    };
+  
+    const markTodo = (id) => {
+      const newToDos = toDos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done: !todo.done };
+        }
+        return todo;
+      });
+      setToDos(newToDos);
+    };
+  
+    const completedToDos = toDos.filter((todo) => todo.done);
+    const pendingToDos = toDos.filter((todo) => !todo.done);
 
 
 export const todoContext = createContext()
@@ -6,7 +35,7 @@ export const todoContext = createContext()
 export const ToDoProvider = ({children}) => {
 
     return (
-        <todoContext.Provider value={}>
+        <todoContext.Provider value={{addToDo, markTodo, completedToDos, pendingToDos}}>
                 {children}
         </todoContext.Provider>
     )
